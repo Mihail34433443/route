8398fa0421b7fd455a0bfcde972ff7f294ecfcf4
Mustafa Andich <mustafa.andich@gmail.com>
2021-12-06 17:11:15 +0300

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PriceComponent } from './price/price.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "news", component: NewsComponent },
+  { path: "price", component: PriceComponent },
+  { path: "**", component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
